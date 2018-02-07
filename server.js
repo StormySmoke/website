edb6d5081fe7310d21d3534a2ca253c732795a87
Daniel Weibel <danielmweibel@gmail.com>
2018-02-06 22:56:25 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handle querying of nearest neighbours
+app.post('/query', function(req, res) {
+    res.send(req.body.query + " [PROCESSED]");
+});
+
+// Handle indexing of text
+app.post('/index', function(req, res) {
+    res.send("Text successfully indexed.");
+});
+
+var server = app.listen(8080);
